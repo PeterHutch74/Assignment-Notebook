c838a1529703e5db45767804108b961182b89359
Peter Hutchison <Peterhutch03@gmail.com>
2022-04-18 10:32:40 -0500

    Set up list edit functionality"

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assignmentItems = [AssignmentItem(subject: "Math", assignment: "Page 1-4", dueDate: Date()), AssignmentItem(subject: "English", assignment: "Essay", dueDate: Date()), AssignmentItem(subject: "Science", assignment: "Lab Write Up", dueDate: Date())]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(assignmentItems) { item in
+                    Text(item.assignment
+                    )
+                }
+                .onMove(perform: { indices, newOffset in
+                    assignmentItems.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    assignmentItems.remove(atOffsets: indexSet)
+                })
+            }
+            .navigationBarTitle("Assignment Notebok", displayMode: .inline)
+            .navigationBarItems(leading: EditButton())
+        }
     }
 }
 
@@ -18,4 +33,12 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct AssignmentItem: Identifiable, Codable
+{
+    var id = UUID()
+    var subject = String()
+    var assignment = String()
+    var dueDate = Date()
 }
