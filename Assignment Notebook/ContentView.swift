8e0b3f2c2792752bcfec96afc7bfa26e837377f8
Peter Hutchison <Peterhutch03@gmail.com>
2022-04-22 18:45:07 -0500

    created initial assignment list

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,27 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var things = ["First Thing", "Second Thing", "Third Thing", "Fourth Thing", "Fifth Thing"]
+    @State private var toDoItems =
+        
+        [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+         ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+         ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
     var body: some View {
         NavigationView {
             List {
-                ForEach(things, id: \.self) { thing in
-                    Text(thing)
+                ForEach(toDoItems) { item in
+                    Text(item.description)
                 }
                 .onMove(perform: { indices, newOffset in
-                    things.move(fromOffsets: indices, toOffset: newOffset)
+                    toDoItems.move(fromOffsets: indices, toOffset: newOffset)
                 })
                 .onDelete(perform: { indexSet in
-                    things.remove(atOffsets: indexSet)
+                    toDoItems.remove(atOffsets: indexSet)
                 })
             }
-            .navigationBarTitle("Things", displayMode: .inline)
-            .navigationBarItems(leading: EditButton())
         }
+        .navigationBarTitle("Assignment List", displayMode: .inline)
+        .navigationBarItems(leading: EditButton())
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    struct ToDoItem: Identifiable {
+        var id = UUID()
+        var course = String()
+        var description = String()
+        var dueDate = Date()
+    }
     static var previews: some View {
         ContentView()
     }
