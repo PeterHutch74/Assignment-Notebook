53891e08ffaa2392c939eb178c7abf067d8e7524
Peter Hutchison <Peterhutch03@gmail.com>
2022-04-22 18:38:39 -0500

    Reset up edit functionality

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,21 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var items = ["First Item", "Second Item", "Third Item", "Fourth Item", "Fifth Item"]
+    @State private var things = ["First Thing", "Second Thing", "Third Thing", "Fourth Thing", "Fifth Thing"]
     var body: some View {
         NavigationView {
             List {
-                ForEach(items, id: \.self) { thing in
-                    Text(item)
+                ForEach(things, id: \.self) { thing in
+                    Text(thing)
                 }
                 .onMove(perform: { indices, newOffset in
-                    items.move(fromOffsets: indices, toOffset: newOffset)
+                    things.move(fromOffsets: indices, toOffset: newOffset)
                 })
                 .onDelete(perform: { indexSet in
-                    items.remove(atOffsets: indexSet)
+                    things.remove(atOffsets: indexSet)
                 })
             }
-            .navigationBarTitle("Items", displayMode: .inline)
+            .navigationBarTitle("Things", displayMode: .inline)
             .navigationBarItems(leading: EditButton())
         }
     }
